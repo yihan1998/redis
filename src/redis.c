13ce497045c01a84333c0f605d7c29933c139f2a
Yang Yihan <87106800+yihan1998@users.noreply.github.com>
2021-11-22 00:17:34 +0800

    Update redis.c

--- src/redis.c
+++ src/redis.c
@@ -1690,36 +1690,38 @@ int listenToPort(int port, int *fds, int *count) {
      * entering the loop if j == 0. */
     if (server.bindaddr_count == 0) server.bindaddr[0] = NULL;
     for (j = 0; j < server.bindaddr_count || j == 0; j++) {
-        if (server.bindaddr[j] == NULL) {
-            /* Bind * for both IPv6 and IPv4, we enter here only if
-             * server.bindaddr_count == 0. */
-            fds[*count] = anetTcp6Server(server.neterr,port,NULL,
-                server.tcp_backlog);
-            if (fds[*count] != ANET_ERR) {
-                anetNonBlock(NULL,fds[*count]);
-                (*count)++;
+        // if (server.bindaddr[j] == NULL) {
+        //     /* Bind * for both IPv6 and IPv4, we enter here only if
+        //      * server.bindaddr_count == 0. */
+        //     fds[*count] = anetTcp6Server(server.neterr,port,NULL,
+        //         server.tcp_backlog);
+        //     if (fds[*count] != ANET_ERR) {
+        //         anetNonBlock(NULL,fds[*count]);
+        //         (*count)++;
 
-                /* Bind the IPv4 address as well. */
-                fds[*count] = anetTcpServer(server.neterr,port,NULL,
-                    server.tcp_backlog);
-                if (fds[*count] != ANET_ERR) {
-                    anetNonBlock(NULL,fds[*count]);
-                    (*count)++;
-                }
-            }
-            /* Exit the loop if we were able to bind * on IPv4 and IPv6,
-             * otherwise fds[*count] will be ANET_ERR and we'll print an
-             * error and return to the caller with an error. */
-            if (*count == 2) break;
-        } else if (strchr(server.bindaddr[j],':')) {
-            /* Bind IPv6 address. */
-            fds[*count] = anetTcp6Server(server.neterr,port,server.bindaddr[j],
-                server.tcp_backlog);
-        } else {
-            /* Bind IPv4 address. */
-            fds[*count] = anetTcpServer(server.neterr,port,server.bindaddr[j],
-                server.tcp_backlog);
-        }
+        //         /* Bind the IPv4 address as well. */
+        //         fds[*count] = anetTcpServer(server.neterr,port,NULL,
+        //             server.tcp_backlog);
+        //         if (fds[*count] != ANET_ERR) {
+        //             anetNonBlock(NULL,fds[*count]);
+        //             (*count)++;
+        //         }
+        //     }
+        //     /* Exit the loop if we were able to bind * on IPv4 and IPv6,
+        //      * otherwise fds[*count] will be ANET_ERR and we'll print an
+        //      * error and return to the caller with an error. */
+        //     if (*count == 2) break;
+        // } else if (strchr(server.bindaddr[j],':')) {
+        //     /* Bind IPv6 address. */
+        //     fds[*count] = anetTcp6Server(server.neterr,port,server.bindaddr[j],
+        //         server.tcp_backlog);
+        // } else {
+        //     /* Bind IPv4 address. */
+        //     fds[*count] = anetTcpServer(server.neterr,port,server.bindaddr[j],
+        //         server.tcp_backlog);
+        // }
+        fds[*count] = anetTcpServer(server.neterr,port,server.bindaddr[0],
+                        server.tcp_backlog);
         if (fds[*count] == ANET_ERR) {
             redisLog(REDIS_WARNING,
                 "Creating Server TCP listening socket %s:%d: %s",
