99a7a4237d67f2f69208351fbc7dcbc5497308cd
Yang Yihan <87106800+yihan1998@users.noreply.github.com>
2021-12-14 13:37:38 +0800

    Update anet.c

--- src/anet.c
+++ src/anet.c
@@ -571,7 +571,7 @@ static int anetGenericAccept(char *err, int s, struct sockaddr *sa, socklen_t *l
             fd = -1;
             break;
         }
-        printf(" [%s:%d] socket %d accept connection via %d\n", __func__, __LINE__, s, fd);
+        // printf(" [%s:%d] socket %d accept connection via %d\n", __func__, __LINE__, s, fd);
         break;
     }
     return fd;
